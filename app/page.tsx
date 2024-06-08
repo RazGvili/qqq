d283aaf71ae5bdaac49a23f3819e17d3d16193b8
Raz <razik.gvili@gmail.com>
2024-06-08 13:33:29 +0300

    wee

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 import style from "./app.module.css";
 
-// generate random emojy
 const RandomEmoji = () => {
-  const emojis = ["👋", "👍", "👏", "🤝", "🙌", "🌟", "🎉", "🇮🇱❤️"];
+  const emojis = ["👋", "👍", "👏", "🤝", "🙌", "🌟", "🎉", "🇮🇱"];
   return (
     <div className="h-full w-full text-9xl grid place-items-center">
       {emojis[Math.floor(Math.random() * emojis.length)]}
@@ -13,13 +12,15 @@ const RandomEmoji = () => {
 export default function Home() {
   return (
     <main className="grid grid-row-1 grid-cols-1 place-items-center gap-[50dvh] min-h-screen max-h-screen px-12 py-24 overflow-auto bg-[rgb(20,20,20)]">
-      <span className={style.support}>
-        <code>
-          This browser does not supports experimental animation-timeline: view()
+      <div className={style.support}>
+        <code className="bg-slate-400 text-center p-1">
+          This browser does not support CSS Animation Timeline
         </code>
+        <code> 🙅 animation-timeline: view() 🙅</code>
 
         <div className="animate-bounce text-5xl">😔</div>
-      </span>
+      </div>
+
       {Array.from({ length: 50 }).map((_, i) => (
         <div key={i} className={style.card}>
           <RandomEmoji />
