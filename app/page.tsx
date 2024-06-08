1463a871797ecd7a227e7d37cc870fb50182bff9
Raz <razik.gvili@gmail.com>
2024-06-08 13:43:16 +0300

    try to fix support mobile

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import style from "./app.module.css";
 
 const RandomEmoji = () => {
-  const emojis = ["👋", "👍", "👏", "🤝", "🙌", "🌟", "🎉", "🇮🇱"];
+  const emojis = ["🎉", "🇮🇱"];
   return (
     <div className="h-full w-full text-9xl grid place-items-center">
       {emojis[Math.floor(Math.random() * emojis.length)]}
@@ -11,7 +11,7 @@ const RandomEmoji = () => {
 
 export default function Home() {
   return (
-    <main className="grid grid-row-1 grid-cols-1 place-items-center gap-[50dvh] min-h-screen max-h-screen px-12 py-24 overflow-auto bg-[rgb(20,20,20)]">
+    <main className="grid grid-row-1 grid-cols-1 place-items-center gap-[50dvh] min-h-screen max-h-screen px-12 py-24 overflow-auto bg-[rgb(20,20,20)] min-w-screen">
       <div className={style.support}>
         <code className="bg-slate-400 text-center p-1">
           This browser does not support CSS Animation Timeline
@@ -20,7 +20,6 @@ export default function Home() {
 
         <div className="animate-bounce text-5xl">😔</div>
       </div>
-
       {Array.from({ length: 50 }).map((_, i) => (
         <div key={i} className={style.card}>
           <RandomEmoji />
