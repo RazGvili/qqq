0a0347ac1d38ce9452b4ebb1c948391b98aa8e19
Raz <razik.gvili@gmail.com>
2024-06-08 13:22:04 +0300

    support tag

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,13 @@ const RandomEmoji = () => {
 export default function Home() {
   return (
     <main className="grid grid-row-1 grid-cols-1 place-items-center gap-[50dvh] min-h-screen max-h-screen px-12 py-24 overflow-auto bg-[rgb(20,20,20)]">
+      <span className={style.support}>
+        <code>
+          This browser does not supports experimental animation-timeline: view()
+        </code>
+
+        <div className="animate-bounce text-5xl">😔</div>
+      </span>
       {Array.from({ length: 50 }).map((_, i) => (
         <div key={i} className={style.card}>
           <RandomEmoji />
